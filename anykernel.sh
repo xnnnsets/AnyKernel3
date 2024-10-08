fb1bd2a3aef37b9e3fd4483e4a3c7da356c18add
HiN <solihinprampampam@gmail.com>
2024-10-09 00:24:10 +0700

    ADD

---
+++ anykernel.sh
@@ -0,0 +1,58 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+# Edit by @physwizz Generic 
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=
+do.devicecheck=0
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+'; } # end properties
+
+# shell variables
+block=/dev/block/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+ui_print "- Unpacking boot image";
+
+## AnyKernel install
+dump_boot;
+
+mount /system/
+mount /system_root/
+
+# Change permissions
+chmod 755 /system/bin/busybox;
+
+# Deepsleep fix (@Chainfire)
+for i in `ls /sys/class/scsi_disk/`; do
+	cat /sys/class/scsi_disk/$i/write_protect 2>/dev/null | grep 1 >/dev/null
+	if [ $? -eq 0 ]; then
+		echo 'temporary none' > /sys/class/scsi_disk/$i/cache_type
+	fi
+done;
+
+umount /system;
+umount /system_root;
+
+ui_print "- Installing new boot image";
+
+write_boot;
+
+ui_print "- Done";
+ui_print " ";
+
+## end install
